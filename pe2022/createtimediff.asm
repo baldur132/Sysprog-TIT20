4fa6ada800e01498898e92cc32c724d44998478c
baldur132 <baldur@siegels.us>
2022-04-14 21:09:21 +0200

    Time exports

--- pe2022/createtimediff.asm
+++ pe2022/createtimediff.asm
@@ -40,10 +40,13 @@ SECTION .data
 secs:       db  0
 mins:       db  0
 hours:      db  0
+usecs_adj:  db  0
 days_sep:   db  " days, "
 days_len:   db  7
 day_sep:    db  " day, "
 day_len:    db  6
+time_str:   db  "00:00:00."
+usec_str:   db  "000000"
 
 ;-----------------------------------------------------------------------------
 ; Section TEXT
@@ -70,8 +73,64 @@ createtimediff:
         xor   rdx,rdx
         div   rbx           ; divide to get hours in rdx (and days in rax)
         mov   BYTE[hours],dl
+        mov   rdx,r8        ; free up r8
 
+        ;----- calculate usecs and adjust time -----
+        mov   rcx,[rdx + 8] ; grab current usecs
+        mov   r8,[rsi + 8]  ; grab old usecs
+        cmp   rcx,r8        ; compare old usecs
+        jae   .substract_normal
+        ; current < old, we have to adjust seconds
+        mov   rcx,1000000
+        sub   rcx,r8
+        mov   [usecs_adj],rcx
+        movzx rcx,BYTE[secs]
+        test  rcx,rcx       ; see if we can subtract secs
+        jnz   .subtract_secs
+        ; secs = 0, we have to remove a minute
+        mov   rcx,59
+        movzx rcx,BYTE[mins]
+        test  rcx,rcx
+        jnz   .subtract_mins
+        ; mins = 0, we have to remove an hour
+        mov   rcx,59
+        movzx rcx,BYTE[hours]
+        test  rcx,rcx
+        jnz   .subtract_hours
+        ; hours = 0, we have to remove a day
+        mov   rcx,23
+        mov   rcx,rax
+        jmp   .tostring
+
+.subtract_hours:
+        dec   rcx
+        mov   BYTE[mins],cl
+        jmp   .tostring
+
+.subtract_mins:
+        dec   rcx
+        mov   BYTE[mins],cl
+        jmp   .tostring
+
+.subtract_secs:
+        dec   rcx
+        mov   BYTE[secs],cl
+        jmp   .tostring
+
+.substract_normal:
+        ; current >= old, we can subtract normally
+        sub   rax,[rsi + 8]
+        mov   [usecs_adj],rax
+
+.tostring:
         call  daystostring
+
+        call  insert_time
+
+        call  usecondstostring
+
+        call  export_time
+
         mov   BYTE[rdi + rax],0xA
         inc   rax
 
@@ -82,10 +141,7 @@ daystostring:
         mov   rbx,10   ; divisor
         test  rax,rax
         jnz   .loop
-
-        mov   byte[rdi],'0'
-        mov   rbx,1
-        jmp   .reverse_end
+        jmp   .end_days
 
 .loop:
         test  rax,rax
@@ -145,7 +201,7 @@ daystostring:
         mov   BYTE[rbx + rcx],dl
         test  rcx,rcx
         jnz   .label_loop
-        jmp   .end
+        jmp   .end_days
 
 .insert_label_singular:
         movzx rcx,BYTE[day_len]
@@ -155,13 +211,85 @@ daystostring:
         movzx rdx,BYTE[day_sep + rcx]
         mov   BYTE[rbx + rcx],dl
         loop  .label_loop
-        jmp   .end
+        jmp   .end_days
 
-.end:
+.end_days:
         ret
 
 insert_time:
-;        ret
+        mov   r9,rax     ; save byte counter
+        mov   rbx,10     ; divisor
+        ; hours
+        movzx rax,BYTE[hours]
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; ones place
+        mov   [time_str + 1],dl
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; tens place
+        mov   [time_str],dl
+    
+        ; minutes
+        movzx rax,BYTE[mins]
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; ones place
+        mov   [time_str + 4],dl
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; tens place
+        mov   [time_str + 3],dl
+
+        ; seconds
+        movzx rax,BYTE[secs]
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; ones place
+        mov   [time_str + 7],dl
+        xor   rdx,rdx
+        div   rbx
+        add   dl,'0'     ; tens place
+        mov   [time_str + 6],dl
+
+        mov    rax,r9
+        ret
+
+usecondstostring:
+        mov   r8,rax    ; save byte counter
+        mov   rcx,6     ; loop counter
+        mov   rbx,10    ; divisor
+        test  rax,rax
+        jnz   .loop
+        jmp   .pad
+.loop:
+        test  rax,rax
+        jz    .pad
+        xor   rdx,rdx   ; clear rdx
+        div   rbx
+        add   dl,'0'    ; bring into ascii number space
+        mov   BYTE[usec_str+rcx],dl
+        loop  .loop
+.pad:
+        test  rcx,rcx
+        jz    .end_usecs
+        mov   BYTE[rdi+rcx],'0'
+        loop  .pad
+.end_usecs:
+        mov   r8,rax
+        ret
+
+export_time:
+        mov   rcx,15
+        lea   rbx,[rdi + rax]
+        add   rax,15
+.loop_time:
+        dec   rcx
+        movzx rdx,BYTE[time_str + rcx]
+        mov   BYTE[rbx + rcx],dl
+        test  rcx,rcx
+        jnz   .loop_time
+        ret
 
 func_end:
         pop   rbx
